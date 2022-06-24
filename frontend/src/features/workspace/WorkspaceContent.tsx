d65a4303dd26aaa6bc9356145e9feca7a0b55efb
Hans Tirtaputra <hansybastian@gmail.com>
2022-06-24 15:55:09 +0800

    feat: add mobile view for workspace page

--- frontend/src/features/workspace/WorkspaceContent.tsx
+++ frontend/src/features/workspace/WorkspaceContent.tsx
@@ -134,7 +134,6 @@ export const WorkspaceContent = ({
           borderBottom="1px solid var(--chakra-colors-neutral-300)"
           px="2rem"
           py="1rem"
-          ref={topRef}
         >
           <WorkspaceHeader
             isLoading={isLoading}
